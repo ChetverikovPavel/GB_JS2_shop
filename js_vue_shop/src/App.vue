9ef94a608f2ba5a882b4f1f0f2140fdfe9f7c9e9
Pavel <kas62-96@mail.ru>
2022-01-26 22:02:04 +0300

    lesson_7. Vue.js. Api post & delete added

---
+++ js_vue_shop/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+      <header>
+        <h2>Vue Shop</h2>
+        <nav>
+          <router-link to="/">Каталог</router-link>
+          <router-link to="/cart">Корзина</router-link>
+        </nav>
+      </header>
+      <router-view :showcase="showcase" :cart="cart" v-on:productAdd="addToCart" v-on:productDelete="removeFromCart"></router-view>
+  </div>
+</template>
+
+<script>
+const API_URL = '/api/v1'
+
+export default {
+  data() {
+    return {
+      showcase: [],
+      cart: []
+    }
+  },
+  methods: {
+    addToCart(product) {
+      fetch(`${API_URL}/cart`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      })
+        .then(() => {
+          this.cart.push(product)
+        })
+    },
+
+    removeFromCart(product) {
+      fetch(`${API_URL}/cart`, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      })
+        .then(() => {
+          const index = this.cart.findIndex((item) => item.id == product.id)
+          this.cart.splice(index, 1)
+        })
+    }
+  },
+  mounted() {
+    fetch(`${API_URL}/catalog`)
+      .then((req) => req.json())
+      .then((data) => {
+        this.showcase = data
+      }),
+    
+    fetch(`${API_URL}/cart`)
+      .then((req) => req.json())
+      .then((data) => {
+        this.cart = data
+      })
+  },
+}
+</script>
+
+<style lang="scss">
+  *{
+    padding: 0;
+    margin: 0;
+  }
+  header {
+    height: 80px;
+    padding: 10px 20px;
+    background: #ccc;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+
+    nav {
+      display: flex;
+      justify-content: space-between;
+      width: 50%;
+      align-items: center;
+      
+
+    }
+  }
+</style>
---
+++ js_vue_shop/src/views/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <product-card v-for="product of cart" :key="product.id" :data="product" :target="'cart'" v-on:productAction="onDelete"></product-card>
+  </div>
+</template>
+
+<script>
+import ProductCard from '../components/ProductCard.vue'
+
+export default {
+  components: { ProductCard },
+  name: 'Home',
+  props: [
+    'showcase',
+    'cart'
+  ],
+  methods: {
+    onDelete(product) {
+      this.$emit('productDelete', product)
+    }
+  },
+}
+
+</script>
